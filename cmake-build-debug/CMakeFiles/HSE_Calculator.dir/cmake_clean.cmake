944adb2aff96a74f802823782fb06522e9f02e3d
heartsker <heartsker@gmail.com>
2021-02-13 15:19:45 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/HSE_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HSE_Calculator.dir/main.c.o"
+  "HSE_Calculator"
+  "HSE_Calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HSE_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
